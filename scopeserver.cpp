fb2b774ae715cd260b262509fca407bed8499829
Kjelland <36665616+Kjelland@users.noreply.github.com>
2019-10-08 20:35:00 +0200

    updatet scopeserver

--- scopeserver.cpp
+++ scopeserver.cpp
@@ -2,17 +2,37 @@
 
 ScopeServer::ScopeServer(QObject *parent) : QObject(parent)
 {
+    _haltChartRefresh = false;
 
+    // SETUP LOGGING
+    signalInformation.append(loggerInfo{0,"Voltage",""});
+    signalInformation.append(loggerInfo{1,"Current",""});
+    signalInformation.append(loggerInfo{2,"Power",""});
+    signalInformation.append(loggerInfo{3,"Flux",""});
+    signalInformation.append(loggerInfo{4,"Pressure",""});
+
+    QVector<QPointF> points;
+    points.reserve(20);
+
+    logger.append(points);
+    logger.append(points);
+    logger.append(points);
+    logger.append(points);
+    logger.append(points);
+
+    newDataTimer = new QTimer(this);
+    connect(newDataTimer, SIGNAL(timeout()), this, SLOT(newDataRecived()),Qt::UniqueConnection);
+    newDataTimer->start(10);
 }
 
 void ScopeServer::writeToCSV(const QString & file)
 {
     // Write file
-    qDebug() << "Write to file is not implemented yet";
+    qDebug() << "Write to file is not implemented yet: File: "<<file;
 }
 
 void ScopeServer::readFromCSV(const QString & file)
 {
     // Read file
-    qDebug() << "Read from file is not implemented yet";
+    qDebug() << "Read from file is not implemented yet. File: "<<file;
 }
--- scopeserver.h
+++ scopeserver.h
@@ -3,10 +3,25 @@
 
 #include <QObject>
 #include <QDebug>
-
+#include <QTimer>
 class ScopeServer : public QObject
 {
     Q_OBJECT
+    struct loggerInfo
+    {
+        int index;//index of logger list
+        QString name;//name of signal
+        QString unit;//optional: signal unit
+    };
+
+    QList<loggerInfo> signalInformation;
+    QList<QVector<QPointF>> logger;
+
+    bool _haltChartRefresh;
+    double max;
+    double min;
+    bool _historicData;
+    QTimer* newDataTimer;
 public:
     explicit ScopeServer(QObject *parent = nullptr);
 
@@ -15,6 +30,8 @@ signals:
     void refreshChart();
 
 public slots:
+    // Updates the logger list function
+    void newDataRecived();//For demo
     int getNumberOfSignals() { return 0; }
     void writeToCSV(const QString & file);
     void readFromCSV(const QString & file);
